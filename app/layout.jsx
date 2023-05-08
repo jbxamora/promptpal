cba8a286a90d290a0f2cccd108e237a7900d2fba
jbxamora <jbxamora@icloud.com>
2023-05-08 16:03:37 -0700

    created connection to database and user model.

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Provider>
         <div className="main">
           <div className="gradient" />
         </div>
@@ -19,6 +20,7 @@ const RootLayout = ({ children }) => {
           <Nav />
           {children}
           </main>
+        </Provider>
       </body>
     </html>
   );
