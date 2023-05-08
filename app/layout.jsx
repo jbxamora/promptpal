705ed27735636635aa7d55400446b6e36c3c7228
jbxamora <jbxamora@icloud.com>
2023-05-07 21:02:55 -0700

    app config

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css"
+
+export const metadata = {
+    title: "PromptPal",
+    description: "Discover & Share AI Prompts"
+}
+
+const layout = () => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default layout
