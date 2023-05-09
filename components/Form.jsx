d2799bf46bca0cbdf75ba1ad55ad8617b7b2388f
jbxamora <jbxamora@icloud.com>
2023-05-08 17:28:58 -0700

    create post page

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import Link from "next/link";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="w-full max-w-full flex flex-col items-center">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-center max-w-md">
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform
+      </p>
 
-export default Form
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-300">
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your post here"
+            required
+            className="form_textarea "
+          />
+        </label>
+
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-300">
+            Field of Prompt{" "}
+            <span className="font-normal">
+              (#product, #webdevelopment, #idea, etc.)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type="text"
+            placeholder="#Tag"
+            required
+            className="form_input"
+          />
+        </label>
+
+        <div className="flex-end mx-auto mb-5 gap-4">
+          <Link
+            href="/"
+            className="text-gray-500 text-sm border border-gray-400 rounded-full px-5 py-1.5 hover:bg-blue-200 hover:text-black"
+          >
+            Cancel
+          </Link>
+
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm border border-gray-400 hover:bg-primary-orange rounded-full text-white hover:text-white"
+          >
+            {submitting ? `${type}ing...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
